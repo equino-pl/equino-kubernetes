8533000533be37c7946241f5917a613fa52e6559
Marcin Ciapa <aaryndo@gmail.com>
2022-11-26 23:53:33 +0100

    Install cert manager

--- 02_k3s_first_master_node/01_setup_server_node.sh
+++ 02_k3s_first_master_node/01_setup_server_node.sh
@@ -5,5 +5,3 @@ curl -sfL https://get.k3s.io | sh -s server \
 --cluster-init \
 #--flannel-backend=wireguard \
 --write-kubeconfig-mode 644
-
-#cat ./manifests/traefik-config.yaml | envsubst | kubectl apply -f -

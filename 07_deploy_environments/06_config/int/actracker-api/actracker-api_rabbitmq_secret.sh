b479c816206fac3988e3f5b84718a562d387a1f5
Marcin Ciapa <aaryndo@gmail.com>
2023-03-02 19:34:40 +0100

    Provide RabbitMQ config for actracker-api

---
+++ 07_deploy_environments/06_config/int/actracker-api/actracker-api_rabbitmq_secret.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+  then
+    echo "No arguments supplied. Requires actracker-api-publisher-rabbitmq.username and actracker-api-publisher-rabbitmq.password as arguments"
+    exit -1
+fi
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: actracker-api-rabbitmq-secret
+  namespace: equino-int
+type: Opaque
+data:
+  actracker-api-publisher-rabbitmq.username: $(echo -n "$1" | base64)
+  actracker-api-publisher-rabbitmq.password: $(echo -n "$2" | base64)
+EOF

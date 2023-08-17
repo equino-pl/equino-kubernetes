52340deb6ddb748bc2873c4a92f6891a46280ca1
Marcin Ciapa <aaryndo@gmail.com>
2023-08-17 22:26:38 +0200

    Create Postgres database for actracker-search-feeder

---
+++ 10_postgres/08_actracker-search-feeder_int/02_owner_secret.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+  then
+    echo "No arguments supplied. Requires actracker-search-feeder-int-owner-username and actracker-search-feeder-int-owner-password as arguments"
+    exit -1
+fi
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: actracker-search-feeder-int-owner-secret
+  namespace: equino-postgres
+type: Opaque
+data:
+  OWNER_USERNAME: $(echo -n "$1" | base64)
+  OWNER_PASSWORD: $(echo -n "$2" | base64)
+  DB_NAME: $(echo -n "actracker_search_feeder_int" | base64)
+  SCHEMA_NAME: $(echo -n "actracker_search_feeder_int_schema" | base64)
+EOF
---
+++ 10_postgres/09_actracker-search-feeder_prod/01_postgres_secret.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]
+  then
+    echo "No arguments supplied. Requires actracker-search-feeder-prod-postgres-username and actracker-search-feeder-prod-postgres-password as arguments"
+    exit -1
+fi
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+metadata:
+  name: actracker-search-feeder-prod-postgres-secret
+  namespace: equino-postgres
+type: Opaque
+data:
+  POSTGRES_USERNAME: $(echo -n "$1" | base64)
+  POSTGRES_PASSWORD: $(echo -n "$2" | base64)
+EOF
